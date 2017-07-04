efc2c2f90f30294f4e1542dec9ef88a4887ee079
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-04 17:50:10 +0000

    Add another app.get() to pass urlDatabase variables values

---
+++ express-server-EJS.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "s9m5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+})
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n")
+})
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase}
+  res.render("urls-index", templateVars);
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+module.exports = urlDatabase;
